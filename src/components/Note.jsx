8619171957d377367b21112e9f0f71cac8c0f4af
gokulkumar0209 <84857047+gokulkumar0209@users.noreply.github.com>
2024-06-25 01:13:01 +0530

    Rendered the Static notes

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
-function Note({ id, content }) {
+function Note({ inpos, content }) {
+	// Check if inpos is defined before accessing its properties
+	if (!inpos) {
+		return null; // or handle the case where inpos is not defined
+	}
+
 	return (
 		<div
-			className=" absolute left-0 top-0  border border-solid border-black select-none p-10 w-max cursor-move
-          bg-yellow-100 "
+			className="note absolute border border-solid border-black select-none p-10 w-max cursor-move bg-yellow-100"
+			style={{ left: `${inpos.x}px`, top: `${inpos.y}px` }}
 		>
 			{content}
 		</div>
--- src/components/Notes.jsx
+++ src/components/Notes.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Note from "./Note";
 function Notes({ notes, setNotes }) {
+	useEffect(() => {
+		const savedNotes = [];
+		const updatedNotes = notes.map((note) => {
+			const savedNote = null;
+			if (savedNote) {
+				return note;
+			} else {
+				const position = determinePosition();
+				return { ...note, position };
+			}
+		});
+		setNotes(updatedNotes);
+	}, []);
+
+	const determinePosition = () => {
+		const maxX = window.innerWidth - 250;
+		const maxY = window.innerHeight - 250;
+		return {
+			x: Math.floor(Math.random() * maxX),
+			y: Math.floor(Math.random() * maxY),
+		};
+	};
+
 	return (
 		<div>
 			{notes.map((note) => {
-				return <Note key={note.id} content={note.text} />;
+				return <Note key={note.id} inpos={note.position} content={note.text} />;
 			})}
 		</div>
 	);
